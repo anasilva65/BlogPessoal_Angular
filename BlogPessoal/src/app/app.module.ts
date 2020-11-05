7f015ed08242707cac33520265cb692faa257e1e
unknown <annadasilva89@hotmail.com>
2020-11-04 23:50:29 -0300

    Consumo Api

--- BlogPessoal/src/app/app.module.ts
+++ BlogPessoal/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,10 +8,10 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FeedComponent } from './feed/feed.component';
 import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
-
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
@@ -19,16 +20,17 @@ import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.componen
     NavbarComponent,
     FooterComponent,
     FeedComponent,
-    PerfilLateralComponent
+    PerfilLateralComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    BrowserAnimationsModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
--- BlogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
+++ BlogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-lateral.component.css']
 })
 export class PerfilLateralComponent implements OnInit {
-
+nome: string;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.nome='Ana'
   }
 
 }
